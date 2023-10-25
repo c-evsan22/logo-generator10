e80059d0cb86eeec25866210a0478d95e146fde4
Chris San <christianes322@gmail.com>
2023-10-24 21:48:09 -0500

    added and described the tests for shapes

--- assets/lib/shapes.test.js
+++ assets/lib/shapes.test.js
@@ -1,13 +1,32 @@
 const {Triangle, Square, Circle} = require("./shapes");
 
 describe ("Triangle Test",()=>{
-    test("test for a triangle with a blue background",()=>{
+    test("test for a triangle with a red background",()=>{
         const shape = new Triangle();
         shape.setColor("red");
         expect(shape.render()).toEqual(
-            '<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
+            '<polygon points="150, 18 244, 182 56, 182" fill="red" />'
         );
     });
 });
 
-describe
+describe("Square test",()=>{
+    test("test Square for purple background",()=>{
+        const shape = new Square();
+        shape.setColor("purple");
+        expect(shape.render()).toEqual(
+            '<rect x="73" y="40" width="160" height="160" fill="purple" />'
+        );
+    });
+});
+
+
+describe("Circle test",()=>{
+    test("test Circle with a blue background",()=>{
+        const shape = new Circle();
+        shape.setColor("blue");
+        expect(shape.render()).toEqual(
+            '<circle cx="150" cy="115" r="80" fill="blue" />'
+        )
+    })
+})
